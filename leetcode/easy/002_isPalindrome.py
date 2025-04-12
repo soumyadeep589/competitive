114911c7dbf7f0cba06c6ebf3cadfef33529f11a
Soumyadeep Mondal <soumyadeepmondal@Soumyadeeps-MacBook-Air.local>
2025-04-12 12:56:44 +0530

    add palindrome without converting to string

--- leetcode/easy/002_isPalindrome.py
+++ leetcode/easy/002_isPalindrome.py
@@ -41,3 +41,22 @@ def isPalindrome(x: int) -> bool:
     
 
 isPalindrome(121)
+
+"""
+Without converting to string
+"""
+
+def is_palindrome(x: int) -> bool:
+
+    if x<0:
+        return False
+    original = x
+    reversed_num = 0
+    while x!=0:
+        digit = x % 10
+        reversed_num = reversed_num * 10 + digit
+        x = x // 10
+
+    return original == reversed_num
+
+print(is_palindrome(121))
